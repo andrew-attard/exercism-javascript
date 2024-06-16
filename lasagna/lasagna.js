9f3c159ad987e59942e7034b17862eaed2f7ebd5
Andrew Attard <a2attard@gmail.com>
2024-06-16 17:22:45 +0200

    Updated solution with correct syntax to return values from each function

--- lasagna/lasagna.js
+++ lasagna/lasagna.js
@@ -30,7 +30,7 @@
 //
 // Good luck preparing some lasagna!
 
-const EXPECTED_MINUTES_IN_OVEN = 40;
+export const EXPECTED_MINUTES_IN_OVEN = 40;
 
 /**
  * The number of minutes it takes to prepare a single layer.
@@ -45,7 +45,7 @@ const PREPARATION_MINUTES_PER_LAYER = 2;
  * @returns {number} the number of minutes remaining
  */
 export function remainingMinutesInOven(actualMinutesInOven) {
-  EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
+  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
 }
 
 /**
@@ -55,7 +55,7 @@ export function remainingMinutesInOven(actualMinutesInOven) {
  * @returns {number} the total preparation time
  */
 export function preparationTimeInMinutes(numberOfLayers) {
-  numberOfLayers * PREPARATION_MINUTES_PER_LAYER;
+  return numberOfLayers * PREPARATION_MINUTES_PER_LAYER;
 }
 
 /**
@@ -67,5 +67,5 @@ export function preparationTimeInMinutes(numberOfLayers) {
  * @returns {number} the total working time
  */
 export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
-  preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
+  return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
 }
